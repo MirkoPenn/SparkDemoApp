3f1ede0d37f017304fb456a51790c9d22fce07b0
Mirko Pennone <mirko@pennone.org>
2018-05-18 14:20:05 +0200

    login working

--- SparkDemoApp/SparkDemoApp/LoginViewController.swift
+++ SparkDemoApp/SparkDemoApp/LoginViewController.swift
@@ -24,12 +24,14 @@ class LoginViewController: UIViewController {
     
     @IBAction func loginWithSpark(sender: AnyObject) {
         let clientId = "C60bf884e885ab74370102995e932f073d0c98ce2ff05fe453a203d1f677aeefd"
-        let clientSecret = "4fb103707e416f6bff9dcbe441feeeebd0e6101db4f4beaa1aae312afa7ff78a"
+        let clientSecret = "1a0dd38ae30a990ee1c5ad0618c6db37451d1d413d56d52da48cfeb3659acf50"
         let scope = "spark:all"
-        let redirectUri = "Sparkdemoapp://response"
+        let redirectUri = "SparkDemoApp://response"
         
         let authenticator = OAuthAuthenticator(clientId: clientId, clientSecret: clientSecret, scope: scope, redirectUri: redirectUri)
         let spark = Spark(authenticator: authenticator)
+        
+        print(spark.authenticator.authorized)
         
         if !authenticator.authorized {
             authenticator.authorize(parentViewController: self) { success in
